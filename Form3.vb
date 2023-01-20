a2445e96738ca0fff20ca2d41ec633efe7c6960a
Cozmeh <hemsainath15@gmail.com>
2023-01-20 22:39:56 +0530

    New project Sample layout

--- Form3.vb
+++ Form3.vb
@@ -4,4 +4,15 @@
         Me.Close()
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub DeadlineDuration_ValueChanged(sender As Object, e As EventArgs) Handles DeadlineDuration.ValueChanged
+        If ProjectName.Text <> Nothing Then
+            ProjectGrpBox.Text = ProjectName.Text
+        End If
+    End Sub
 End Class
