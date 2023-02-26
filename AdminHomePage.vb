4c170217c700fb1554d7c02fc4e3cc6a33a3e1e4
Rohan Agarwal <rohandiwan1824@gmail.com>
2023-02-26 20:24:59 +0530

    admin page code reduced

--- AdminHomePage.vb
+++ AdminHomePage.vb
@@ -47,42 +47,51 @@ Public Class AdminHomePage
 
         If Add_ID.Text = Nothing Or Add_EmpName.Text = Nothing Or Add_Designtion.Text = Nothing Or Add_Password.Text = Nothing Then
             MsgBox("Please fill all the details!")
-        Else
-
-            'if designation is employee(E) or manager(M)
-            If Add_Designtion.Text = "E" Or Add_Designtion.Text = "M" Then
-
-                'conformation
-                Dim responce As String = MsgBox("Add '" & Add_ID.Text & "' to the Company", vbYesNo, "Add Employee?")
-                If responce = vbYes Then
-                    'adds data to the table
-                    Dim AddCommand As String = "INSERT INTO EMPLOYEES (Id,empName,Designation,password) VALUES ('" + Add_ID.Text + "','" + Add_EmpName.Text + "','" + Add_Designtion.Text + "','" + Add_Password.Text + "')"
-
-                    'creating a sql command statement 
-                    Dim command As SqlCommand = LoginForm.sql.CreateCommand()
-                    command.CommandText = AddCommand
-
-                    'sqladapter to handle the sql commands 
-                    Dim sqlAdapter As New SqlDataAdapter With {
-                    .SelectCommand = command
-                    }
-
-                    'creates a table with the required data
-                    Dim data As New DataSet()
-                    sqlAdapter.Fill(data)
-
-                    DataLoader()
-
-                    'empty the textboxes
-                    Add_ID.Text = Nothing
-                    Add_EmpName.Text = Nothing
-                    Add_Designtion.Text = Nothing
-                    Add_Password.Text = Nothing
-                End If
-            Else
-                MsgBox("Invalid Designation!")
-            End If
+            Return
         End If
+
+        'designation validation
+        If Add_Designtion.Text <> "A" Or Add_Designtion.Text <> "E" Or Add_Designtion.Text <> "M" Then
+            MsgBox("Invalid Designation!")
+            Return
+        End If
+
+        'confirmation
+        Dim responce As String = vbNo
+        If Add_Designtion.Text = "A" Then
+            responce = MsgBox("Add '" & Add_ID.Text & "' to the Company", vbYesNo, "Adding new Admin?")
+        ElseIf Add_Designtion.Text = "M" Then
+            responce = MsgBox("Add '" & Add_ID.Text & "' to the Company", vbYesNo, "Add Manager?")
+        ElseIf Add_Designtion.Text = "E" Then
+            responce = MsgBox("Add '" & Add_ID.Text & "' to the Company", vbYesNo, "Add Employee?")
+        End If
+        If responce = vbNo Then
+            Return
+        End If
+
+        'adds data to the table
+        Dim AddCommand As String = "INSERT INTO EMPLOYEES (Id,empName,Designation,password) VALUES ('" + Add_ID.Text + "','" + Add_EmpName.Text + "','" + Add_Designtion.Text + "','" + Add_Password.Text + "')"
+
+        'creating a sql command statement 
+        Dim command As SqlCommand = LoginForm.sql.CreateCommand()
+        command.CommandText = AddCommand
+
+        'sqladapter to handle the sql commands 
+        Dim sqlAdapter As New SqlDataAdapter With {
+        .SelectCommand = command
+        }
+
+        'creates a table with the required data
+        Dim data As New DataSet()
+        sqlAdapter.Fill(data)
+
+        DataLoader()
+
+        'empty the textboxes
+        Add_ID.Text = Nothing
+        Add_EmpName.Text = Nothing
+        Add_Designtion.Text = Nothing
+        Add_Password.Text = Nothing
     End Sub
 
     Private Sub DataLoader()
@@ -106,110 +115,116 @@ Public Class AdminHomePage
     Private Sub DeleteRow_Click_1(sender As Object, e As EventArgs) Handles DeleteRow.Click
         If Del_ID.Text = Nothing Then
             MsgBox("Please mention the UserID ")
-        Else
-            'conformation
-            Dim responce As String = MsgBox("Remove access to the Project Management System for '" & Del_ID.Text & "'", vbYesNo, "Remove User?")
-            If responce = vbYes Then
-                Dim DelCommand As String = "DELETE FROM Employees WHERE Id ='" + Del_ID.Text + "'"
-
-                'creating a sql command statement 
-                Dim command As SqlCommand = LoginForm.sql.CreateCommand()
-                command.CommandText = "select * from Employees where Id ='" + Del_ID.Text + "'"
-
-                'sqladapter to handle the sql commands 
-                Dim sqlAdapter As New SqlDataAdapter With {
-                .SelectCommand = command
-                }
-
-                'creates a table with the required data
-                Dim data As New DataSet()
-                sqlAdapter.Fill(data)
-
-                If (data.Tables(0).Rows.Count) > 0 Then
-                    command.CommandText = DelCommand
-                    sqlAdapter.SelectCommand = command
-                    sqlAdapter.Fill(data)
-                    DataLoader()
-                Else
-                    MsgBox("User not found!")
-                End If
-                'reseting textbox
-                Del_ID.Text = Nothing
-            End If
+            Return
         End If
+
+        'conformation
+        Dim responce As String = MsgBox("Remove access to the Project Management System for '" & Del_ID.Text & "'", vbYesNo, "Remove User?")
+        If responce = vbNo Then
+            Return
+        End If
+
+        Dim DelCommand As String = "DELETE FROM Employees WHERE Id ='" + Del_ID.Text + "'"
+
+        'creating a sql command statement 
+        Dim command As SqlCommand = LoginForm.sql.CreateCommand()
+        command.CommandText = "select * from Employees where Id ='" + Del_ID.Text + "'"
+
+        'sqladapter to handle the sql commands 
+        Dim sqlAdapter As New SqlDataAdapter With {
+            .SelectCommand = command
+        }
+
+        'creates a table with the required data
+        Dim data As New DataSet()
+        sqlAdapter.Fill(data)
+
+        If (data.Tables(0).Rows.Count) > 0 Then
+            command.CommandText = DelCommand
+            sqlAdapter.SelectCommand = command
+            sqlAdapter.Fill(data)
+            DataLoader()
+        Else
+            MsgBox("User not found!")
+        End If
+        'reseting textbox
+        Del_ID.Text = Nothing
     End Sub
 
     Private Sub UpdateRow_Click_1(sender As Object, e As EventArgs) Handles UpdateRow.Click
         If Upd_Name.Text = Nothing Or Upd_Designation.Text = Nothing Or Upd_Password.Text = Nothing Then
             MsgBox("Please fill all the details!")
-        Else
-            If Upd_Designation.Text = "E" Or Upd_Designation.Text = "M" Then
-
-                'conformation
-                Dim responce As String = MsgBox("Update Employee?", vbYesNo, "Update " & Upd_ID.Text & " in the database")
-                If responce = vbYes Then
-
-                    Dim UpdateCommand As String = "UPDATE Employees SET empName ='" + Upd_Name.Text + "',Designation ='" + Upd_Designation.Text + "',password='" + Upd_Password.Text + "' Where Id ='" + Upd_ID.Text + "'"
-                    'creating a sql command statement 
-                    Dim command As SqlCommand = LoginForm.sql.CreateCommand()
-                    command.CommandText = UpdateCommand
-
-                    'sqladapter to handle the sql commands 
-                    Dim sqlAdapter As New SqlDataAdapter With {
-                    .SelectCommand = command
-                    }
-
-                    'creates a table with the required data
-                    Dim data As New DataSet()
-                    sqlAdapter.Fill(data)
-
-                    DataLoader()
-
-                    'reseting textbox
-                    Upd_Name.Text = Nothing
-                    Upd_Designation.Text = Nothing
-                    Upd_Password.Text = Nothing
-                    Upd_ID.Text = Nothing
-
-                    Upd_Name.Enabled = False
-                    Upd_Designation.Enabled = False
-                    Upd_Password.Enabled = False
-                    UpdateRow.Enabled = False
-                End If
-            Else
-                MsgBox("Invalid Designation!")
-            End If
+            Return
         End If
+
+        If Upd_Designation.Text <> "E" Or Upd_Designation.Text <> "M" Then
+            MsgBox("Invalid Designation!")
+            Return
+        End If
+
+        'conformation
+        Dim responce As String = MsgBox("Update Employee?", vbYesNo, "Update " & Upd_ID.Text & " in the database")
+        If responce = vbNo Then
+            Return
+        End If
+
+        Dim UpdateCommand As String = "UPDATE Employees SET empName ='" + Upd_Name.Text + "',Designation ='" + Upd_Designation.Text + "',password='" + Upd_Password.Text + "' Where Id ='" + Upd_ID.Text + "'"
+        'creating a sql command statement 
+        Dim command As SqlCommand = LoginForm.sql.CreateCommand()
+        command.CommandText = UpdateCommand
+
+        'sqladapter to handle the sql commands 
+        Dim sqlAdapter As New SqlDataAdapter With {
+             .SelectCommand = command
+        }
+
+        'creates a table with the required data
+        Dim data As New DataSet()
+        sqlAdapter.Fill(data)
+
+        DataLoader()
+
+        'reseting textbox
+        Upd_Name.Text = Nothing
+        Upd_Designation.Text = Nothing
+        Upd_Password.Text = Nothing
+        Upd_ID.Text = Nothing
+
+        Upd_Name.Enabled = False
+        Upd_Designation.Enabled = False
+        Upd_Password.Enabled = False
+        UpdateRow.Enabled = False
     End Sub
 
     Private Sub GetBtn_Click_1(sender As Object, e As EventArgs) Handles GetBtn.Click
         If Upd_ID.Text = Nothing Then
-            MsgBox("Please mention the UserID!")
+            MsgBox("Please mention correct UserID!")
+            Return
+        End If
+
+        'creating a sql command statement 
+        Dim command As SqlCommand = LoginForm.sql.CreateCommand()
+        command.CommandText = "select empName,Designation,password from Employees where Id ='" + Upd_ID.Text + "'"
+
+        'sqladapter to handle the sql commands 
+        Dim sqlAdapter As New SqlDataAdapter With {
+            .SelectCommand = command
+        }
+        'creates a table with the required data
+        Dim data As New DataSet()
+        sqlAdapter.Fill(data)
+
+        If (data.Tables(0).Rows.Count) > 0 Then
+            Upd_Name.Text = data.Tables(0).Rows(0)(0).ToString
+            Upd_Designation.Text = data.Tables(0).Rows(0)(1).ToString
+            Upd_Password.Text = data.Tables(0).Rows(0)(2).ToString
+
+            Upd_Name.Enabled = True
+            Upd_Designation.Enabled = True
+            Upd_Password.Enabled = True
+            UpdateRow.Enabled = True
         Else
-            'creating a sql command statement 
-            Dim command As SqlCommand = LoginForm.sql.CreateCommand()
-            command.CommandText = "select empName,Designation,password from Employees where Id ='" + Upd_ID.Text + "'"
-
-            'sqladapter to handle the sql commands 
-            Dim sqlAdapter As New SqlDataAdapter With {
-                .SelectCommand = command
-            }
-            'creates a table with the required data
-            Dim data As New DataSet()
-            sqlAdapter.Fill(data)
-
-            If (data.Tables(0).Rows.Count) > 0 Then
-                Upd_Name.Text = data.Tables(0).Rows(0)(0).ToString
-                Upd_Designation.Text = data.Tables(0).Rows(0)(1).ToString
-                Upd_Password.Text = data.Tables(0).Rows(0)(2).ToString
-
-                Upd_Name.Enabled = True
-                Upd_Designation.Enabled = True
-                Upd_Password.Enabled = True
-                UpdateRow.Enabled = True
-            Else
-                MsgBox("User not found!")
-            End If
+            MsgBox("User not found!")
         End If
     End Sub
 
