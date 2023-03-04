1200aa41cb20220b37eaa90f394c87a0ac04dfc9
Rohan Agarwal <rohandiwan1824@gmail.com>
2023-03-04 19:12:44 +0530

    veriables created in the projectLayout page

--- ProjectLayout.vb
+++ ProjectLayout.vb
@@ -1,5 +1,9 @@
-﻿Public Class ProjectLayout
-    Private Sub ProjectLayout_Load(sender As Object, e As EventArgs)
+﻿Imports System.Data.SqlClient
+
+Public Class ProjectLayout
+    Private startdate, reqana, Design, Development, Testing, Deadline As Date
+
+    Private Sub ProjectLayout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         'initial positions of panels this block of code will go inside the else block... when the page open for the first time
         LeftDesPanel.Size = ReqPanel.Size
@@ -13,6 +17,8 @@
         'LeftDevPanel.Size = LeftDesPanel.Size + DesPanel.Size
         'LeftTestPanel.Size = LeftDevPanel.Size + DevelopmentPanel.Size
 
+        'store dates as the project loads and will be change when the SIZECHANGE function is called
+        'startdate, reqana, Design, Development, Testing, Deadline
 
         ManagerHomePage.Hide()
         'Filling all the fields when opened from clicking on Datagrid in ManagerHomePage
@@ -33,74 +39,85 @@
     End Sub
 
     'Project layout will be saved when Save button is clicked
-    Private Sub Save_Click(sender As Object, e As EventArgs)
-        Dim responce As String = MsgBox("Do you want to save changes?", vbYesNo, "Are you sure?")
+    Private Sub Save_Click(sender As Object, e As EventArgs) Handles Save.Click
+
+        'LayoutTable()
+
+        Dim responce As String = MsgBox("The changes are saved.\n Do you want to exit?", vbYesNo, "Are you sure?")
         If responce = vbYes Then
             'will store the project layout in the layout table
             Me.Close()
         End If
     End Sub
 
-    Private Sub Edit_Click(sender As Object, e As EventArgs)
+    Private Sub Edit_Click(sender As Object, e As EventArgs) Handles Edit.Click
         EditProjectDetails.Show()
         Me.Enabled = False
         Me.Hide()
     End Sub
 
-    Private Sub AddEmp_Click(sender As Object, e As EventArgs)
+    Private Sub AddEmp_Click(sender As Object, e As EventArgs) Handles addEmp.Click
         Me.Enabled = False
         Contributors.Show()
     End Sub
 
-    Private Sub ProjectLayout_FormClosing_1(sender As Object, e As FormClosingEventArgs)
+    Private Sub ProjectLayout_FormClosing_1(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        'saves the dates in the table
+        'LayoutTable()
+        MsgBox("The dates have been saved.")
+
         ManagerHomePage.Enabled = True
         ManagerHomePage.Show()
         ManagerHomePage.Refresh()
         ManagerHomePage.ManagerDataGrid.Refresh()
     End Sub
 
-    Private Sub ReqSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs)
+    Private Sub ReqSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles ReqSplitter.SplitterMoved
         LeftDesPanel.Size = ReqPanel.Size
         LeftDevPanel.Size = LeftDesPanel.Size + DesPanel.Size
         LeftTestPanel.Size = LeftDevPanel.Size + DevelopmentPanel.Size
         DeployPanel.Size = RightTestPanel.Size
     End Sub
 
-    Private Sub RightDesSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs)
+    Private Sub RightDesSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles RightDesSplitterSplitter.SplitterMoved
         LeftDevPanel.Size = LeftDesPanel.Size + DesPanel.Size
         LeftTestPanel.Size = LeftDevPanel.Size + DevelopmentPanel.Size
         DeployPanel.Size = RightTestPanel.Size
     End Sub
 
-    Private Sub RDevSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs)
+    Private Sub RDevSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles RDevSplitter.SplitterMoved
         LeftTestPanel.Size = LeftDevPanel.Size + DevelopmentPanel.Size
         DeployPanel.Size = RightTestPanel.Size
     End Sub
 
-    Private Sub RightTestSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs)
+    Private Sub RightTestSplitter_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles RightTestSplitter.SplitterMoved
         DeployPanel.Size = RightTestPanel.Size
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         LeftDesPanel.Size = ReqPanel.Size
         LeftDevPanel.Size = LeftDesPanel.Size + DesPanel.Size
         LeftTestPanel.Size = LeftDevPanel.Size + DevelopmentPanel.Size
         DeployPanel.Size = RightTestPanel.Size
     End Sub
 
-    Private Sub RA_Click(sender As Object, e As EventArgs)
+    Private Sub LayoutTable()
+        'adds data to the layout table
+        Dim UpdateCommand As String = "UPDATE Layout SET StartDate ='" + startdate + "',Requirement Analysis ='" + reqana + "',Design='" + Design + "',Developmene='" + Development + "',Testing='" + Testing + "',Deadline='" + Deadline + "' Where PId ='" + ProjectId.Text + "'"
 
+        'creating a sql command statement 
+        Dim command As SqlCommand = LoginForm.sql.CreateCommand()
+        command.CommandText = UpdateCommand
+
+        'sqladapter to handle the sql commands 
+        Dim sqlAdapter As New SqlDataAdapter With {
+             .SelectCommand = command
+        }
+
+        'creates a table with the required data
+        Dim data As New DataSet()
+        sqlAdapter.Fill(data)
     End Sub
 
-    Private Sub DesignBtn_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub DevBtn_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub TestBtn_Click(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
